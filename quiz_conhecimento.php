4018bb326462d7bebb3497b0464b2140c92e6443
Pamela Grandizolli <95643354+PamelaFortes@users.noreply.github.com>
2025-11-04 11:00:11 -0400

    style.css alinhado
    ajustar navbar na prox aula

--- quiz_conhecimento.php
+++ quiz_conhecimento.php
@@ -10,31 +10,31 @@
         <h1>Quiz de Conhecimentos Gerais</h1>
 
         <form action="resultado.php" method="POST">
-            <h3>1) Qual é a capital do Brasil?</h3>
+            <h3>1) pergunta1</h3>
             <input type="radio" name="q1" value="a"> São Paulo <br>
             <input type="radio" name="q1" value="b"> Rio de Janeiro <br>
             <input type="radio" name="q1" value="c"> Brasília <br>
             <input type="radio" name="q1" value="d"> Belo Horizonte <br>
 
-            <h3>2) Quem pintou a Monalisa? </h3>
+            <h3>2) pergunta2 </h3>
             <input type="radio" name="q2" value="a"> Michelangelo <br>
             <input type="radio" name="q2" value="b"> Leonardo da Vinci <br>
             <input type="radio" name="q2" value="c"> Picasso <br>
             <input type="radio" name="q2" value="d"> Van Gogh <br>
 
-            <h3>3) Qual é o maior planeta do Sistema Solar? </h3>
+            <h3>3) pergunta3 </h3>
             <input type="radio" name="q3" value="a"> Marte <br>
             <input type="radio" name="q3" value="b"> Júpiter <br>
             <input type="radio" name="q3" value="c"> Saturno <br>
             <input type="radio" name="q3" value="d"> Sol <br>
 
-            <h3>4) Em que continente fica o Egito? </h3>
+            <h3>4) pergunta4 </h3>
             <input type="radio" name="q4" value="a"> Europa <br>
             <input type="radio" name="q4" value="b"> Ásia <br>
             <input type="radio" name="q4" value="c"> África <br>
             <input type="radio" name="q4" value="d"> América <br>
 
-            <h3>5) Qual é a linguagem de programação usada no lado do servidor? </h3>
+            <h3>5) pergunta5 </h3>
             <input type="radio" name="q5" value="a"> HTML <br>
             <input type="radio" name="q5" value="b"> CSS <br>
             <input type="radio" name="q5" value="c"> PHP <br>
